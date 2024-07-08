cfdb6ccb0a035e9562fb653181b7b2d57ebdd1d1
Yakumo <2092891671@qq.com>
2024-07-08 11:20:02 +0800

    开始界面完成

---
+++ Assets/Scripts/Game/BeginScene/BasePanel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePanel<T> : MonoBehaviour where T : class
+{
+    private static T instance;
+
+    public static T Instance => instance;
+
+    private void Awake()
+    {
+        instance = this as T;
+    }
+
+    public virtual void ShowMe()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public virtual void HideMe()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Game/BeginScene/BeginPanel.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeginPanel : BasePanel<BeginPanel>
+{
+    public CustomGUIButton btnBegin;
+    public CustomGUIButton btnSetting;
+    public CustomGUIButton btnQuit;
+    public CustomGUIButton btnRank;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnBegin.clickEvent += () =>
+        {
+            SceneManager.LoadScene("GameScene");
+        };
+        btnSetting.clickEvent += () =>
+        {
+
+        };
+        btnQuit.clickEvent += () =>
+        {
+            Application.Quit();
+        };
+        btnRank.clickEvent += () =>
+        {
+
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
